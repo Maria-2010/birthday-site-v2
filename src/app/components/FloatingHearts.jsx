c2b94f7f7ae7d3b2d5d55c76e1386d6784eb382c
Maria-2010 <clainasherinmaria@gmail.com>
2026-02-27 22:14:44 +0530

    Update FloatingHearts.jsx

--- src/app/components/FloatingHearts.jsx
+++ src/app/components/FloatingHearts.jsx
@@ -11,17 +11,10 @@ export default function FloatingHearts() {
         <motion.div
           key={i}
           className="absolute text-pink-200/60 text-xl"
-          initial={{
-            y: "100vh",
-            x: Math.random() * 100 + "vw",
-            opacity: 0,
-          }}
-          animate={{
-            y: "-15vh",
-            opacity: [0, 1, 0],
-          }}
+          initial={{ y: "100vh", x: Math.random() * 100 + "vw", opacity: 0 }}
+          animate={{ y: "-15vh", opacity: [0, 1, 0] }}
           transition={{
-            duration: 12 + Math.random() * 8,
+            duration: 10 + Math.random() * 8,
             repeat: Infinity,
             delay: Math.random() * 5,
           }}
