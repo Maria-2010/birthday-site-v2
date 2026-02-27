9e5b163408fc53dbbfdf37c1341f5da9553bdfd0
Maria-2010 <clainasherinmaria@gmail.com>
2026-02-27 22:58:16 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "❤️𝙷𝚊𝚙𝚙𝚢 𝙱𝚒𝚛𝚝𝚑𝚍𝚊𝚢 𝙼𝚞𝚖𝚖𝚢!❤️",
+  title: "❤️𝙷𝚊𝚙𝚙𝚢 𝙱𝚒𝚛𝚝𝚑𝚍𝚊𝚢 𝙼a'am!❤️",
   description: "🎉An animated birthday surprise filled with emotions, words from the heart, and a letter that types itself — just for you.🎉"
 };
 
