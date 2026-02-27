44181867dae9da7324166b93522e948eab46f515
Maria-2010 <clainasherinmaria@gmail.com>
2026-02-27 23:00:02 +0530

    Update HappyBirthday.jsx

--- src/app/components/HappyBirthday.jsx
+++ src/app/components/HappyBirthday.jsx
@@ -220,7 +220,7 @@ export default function HappyBirthday({ onNext }) {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 1.3 }}
                 >
-                    💖Mummy<span className="text-white">💕</span>
+                    💖Ma'am<span className="text-white">💕</span>
                 </motion.h2>
 
                 <motion.div
